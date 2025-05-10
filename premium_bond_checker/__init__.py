8dec6271dfdf8402938aaea8b1db6d9bd0173f5f
Malachi Soord <inverse.chi@gmail.com>
2025-05-10 10:31:50 +0200

    Simplify API (#107)

--- premium_bond_checker/__init__.py
+++ premium_bond_checker/__init__.py
@@ -1,3 +1,11 @@
-from .client import Client
+from .client import BondPeriod, Client
+from .exceptions import InvalidHolderNumberException
+from .models import CheckResult, Result
 
-__all__ = ["Client"]
+__all__ = [
+    "Client",
+    "BondPeriod",
+    "CheckResult",
+    "Result",
+    "InvalidHolderNumberException",
+]
